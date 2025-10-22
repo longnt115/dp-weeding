f2dfe6e7104dc7085dd6c77a6057f3763d94c35a
LONG NGUYEN THANH <longnguyenthanh@LONGs-MacBook-Pro.local>
2025-10-22 21:11:28 +0700

    feat: update hide component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import Countdown from '@/components/Countdown';
 import Hero from '@/components/Hero';
 import OurStory from '@/components/OurStory';
-import RSVPForm from '@/components/RSVPForm';
-import WeddingGifts from '@/components/WeddingGifts';
 
 export default function Home() {
   return (
@@ -14,8 +12,8 @@ export default function Home() {
       <Countdown />
       {/* <Events /> */}
       {/* <LiveStream /> */}
-      <RSVPForm />
-      <WeddingGifts />
+      {/* <RSVPForm />
+      <WeddingGifts /> */}
     </main>
   );
 }
